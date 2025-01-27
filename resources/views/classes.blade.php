8a2b3c2e698c76ac28aee66ec9c00d905e5099c7
Petr Nikitin <petr.niki.777@gmail.com>
2025-01-27 09:40:49 +0300

    переход на sass

--- resources/views/classes.blade.php
+++ resources/views/classes.blade.php
@@ -63,8 +63,8 @@
                         <td class="table__td">{{ $row->child_table2 }}</td> --}}
                         <td class="table__td table__td--wide">{{ $row->description }}</td>
                         <td class="table__td">{{ $row->subsystem }}</td>
-                        <td class="table__td table__td--wide">
-                            <a class="table__link" target="_blank" href="{{ $row->wiki_link }}">{{ $row->wiki_link }}</a>
+                        <td class="table__td">
+                            <a class="table__link" target="_blank" href="{{ $row->wiki_link }}">c_{{ $row->class_id }}</a>
                         </td>
                         <td class="table__td">{{ $row->load_method }}</td>
                         {{-- <td class="table__td">{{ $row->last_updated_dev }}</td> --}}
