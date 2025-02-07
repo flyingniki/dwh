a5759ffeb4d0258177fa98f45f602d8e67c2fcde
Petr Nikitin <flying_niki@mail.ru>
2025-02-07 12:59:17 +0300

    vue js package

--- resources/views/classes.blade.php
+++ resources/views/classes.blade.php
@@ -33,18 +33,14 @@
                 <tr class="table__tr">
                     <th class="table__th">ID</th>
                     <th class="table__th">Название</th>
-                    {{-- <th class="table__th">Направление</th> --}}
                     <th class="table__th">Источник</th>
                     <th class="table__th">Получатель</th>
                     <th class="table__th">DWH(stage)</th>
                     <th class="table__th">DWH(core)</th>
-                    {{-- <th class="table__th">child_table1</th>
-                <th class="table__th">child_table2</th> --}}
                     <th class="table__th">Описание</th>
                     <th class="table__th">Subsystem</th>
                     <th class="table__th">Wiki</th>
                     <th class="table__th">Метод загрузки</th>
-                    {{-- <th class="table__th">last_updated_dev</th> --}}
                 </tr>
             </thead>
             <tbody class="table__body">
@@ -55,13 +51,10 @@
                                 href="{{ route('detail.class', ['metaClassesView' => $row->class_id]) }}">{{ $row->class_id }}</a>
                         </td>
                         <td class="table__td">{{ $row->name }}</td>
-                        {{-- <td class="table__td">{{ $row->in_out }}</td> --}}
                         <td class="table__td">{{ $row->source }}</td>
                         <td class="table__td">{{ $row->reciever }}</td>
                         <td class="table__td table__td--wide">{{ $row->stage_table }}</td>
                         <td class="table__td table__td--wide">{{ $row->core_table }}</td>
-                        {{-- <td class="table__td">{{ $row->child_table1 }}</td>
-                        <td class="table__td">{{ $row->child_table2 }}</td> --}}
                         <td class="table__td table__td--wide">{{ $row->description }}</td>
                         <td class="table__td">{{ $row->subsystem }}</td>
                         <td class="table__td">
@@ -69,7 +62,6 @@
                                 href="{{ $row->wiki_link ?? '' }}">{{ $row->wiki_link ? 'C' . $row->class_id : '' }}</a>
                         </td>
                         <td class="table__td">{{ $row->load_method }}</td>
-                        {{-- <td class="table__td">{{ $row->last_updated_dev }}</td> --}}
                     </tr>
                 @endforeach
             </tbody>
